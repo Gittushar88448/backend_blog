684cf7e5040b9fac3e2fe7026b0882bb1ca39555
Tushar <kmrtushar01@gmail.com>
2024-04-30 20:12:12 +0530

    made the connection for database

---
+++ config/dbConnect.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose')
+require('dotenv').config();
+
+const dbConnect = () =>{
+
+    mongoose.connect(process.env.DATABASE_URL, {
+        useNewUrlParser : true ,
+        useUnifiedTopology : true
+    })
+    .then(()=>{
+        console.log("Mongoose connected successfully")
+    })
+    .catch((error) =>{
+    console.log("error occur " + error)
+    process.exit(1)
+    })
+}
+
+module.exports = dbConnect;
