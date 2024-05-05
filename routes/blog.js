f7a8427538ee53b287d517454ae2aa285ccd46e2
Tushar <kmrtushar01@gmail.com>
2024-05-05 21:42:07 +0530

    creating route for comment

--- routes/blog.js
+++ routes/blog.js
@@ -3,8 +3,10 @@ const router = express.Router();
 
 
 const {createBlog , getAllPosts} = require('../controllers/postController');
+const {createComment} = require('../controllers/commentController');
 
 
 router.post('/posts/create' , createBlog);
+router.post("/comments/create" , createComment);
 
 module.exports = router;
