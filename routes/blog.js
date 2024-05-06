d4b48ff52d9a81cec8ca6ddaa39e688906d8210f
Tushar <kmrtushar01@gmail.com>
2024-05-06 22:22:33 +0530

    adding like post route

--- routes/blog.js
+++ routes/blog.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-
+const {likePost} = require('../controllers/likeController');
 const {createBlog , getAllPosts} = require('../controllers/postController');
 const {createComment} = require('../controllers/commentController');
 
@@ -9,5 +9,8 @@ const {createComment} = require('../controllers/commentController');
 router.post('/posts/create' , createBlog);
 router.post("/comments/create" , createComment);
 router.get('/posts' , getAllPosts);
+router.post("/likes/like",likePost)
+
+
 
 module.exports = router;
